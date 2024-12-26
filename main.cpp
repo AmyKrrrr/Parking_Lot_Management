a696143e9efa7178bdeced3756dda35f72fb534c
Amitesh Kar <amiteshkar@gmail.com>
2024-12-26 19:56:36 +0530

    Declared functions to be used

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
 
+void Main_Menu();
+void Admin_Login();
+    void Admin_Menu();
+        void Check_Visitor_History();
+        void Edit_Entry_Exit_Details();
+        void Black_List_Customer();
+        void Edit_Parking_Availability();
+void Visitor_Login();
+void createVisitorAccount();
+    void Visitor_Menu();
+        void Check_Empty_Parking_Lots();
+        void Book_Parking_Lot();
+        void Input_Entry_Exit_Time();
+        void Check_Past_Visits();
+
 int main()
 {
     
